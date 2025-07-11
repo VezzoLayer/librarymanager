65e7223c977d354abe14b3094380f95808009383
Jacopo Vezzosi <jacopovezzosiwork@gmail.com>
2025-07-11 17:25:45 +0200

    Configurazione iniziale con spring boot setup

---
+++ src/test/java/com/library/librarymanager/LibrarymanagerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.library.librarymanager;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
